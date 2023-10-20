cd7376c100b2de7f5b7a9c4f589383b4705296e2
Ty <ty.hranac@gmail.com>
2023-10-19 17:12:19 -0700

    Add progress indicator

--- main.cc
+++ main.cc
@@ -13,6 +13,7 @@ int main() {
 
     for (int j =0; j < image_height; ++j)
     {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i)
         {
             auto r = double(i) / (image_width - 1);
@@ -26,4 +27,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                      \n";
 }
